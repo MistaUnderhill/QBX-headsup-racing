dafc26a92e23983de73ed794dec4975ae717d45e
MistaUnderhill <110700115+MistaUnderhill@users.noreply.github.com>
2025-07-21 14:17:09 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mistaunderhill'
+description 'Multiplayer Street Race Script for QBX'
+version '2.0.0'
+
+shared_script 'config.lua'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua',
+}
+
+client_scripts {
+    'client.lua',
+}
