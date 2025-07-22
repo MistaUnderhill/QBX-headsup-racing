d8277f64e0fb8bb94d5a1d169c2742ebf04bf0d8
MistaUnderhill <110700115+MistaUnderhill@users.noreply.github.com>
2025-07-22 10:19:44 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,10 @@ author 'Mistaunderhill'
 description 'Multiplayer Street Race Script for QBX'
 version '2.0.0'
 
+-- Register with qbx-core metadata
+resource_type 'gametype' { name = 'StreetRace' }
+dependency 'qbx-core'
+
 shared_script 'config.lua'
 
 server_scripts {
