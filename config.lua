8520699d17ecab669b19ce0106e684e8f7f389ae
MistaUnderhill <110700115+MistaUnderhill@users.noreply.github.com>
2025-07-22 10:00:32 -0700

    Update config.lua
    added min/max buy in

--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 Config = {}
 
-Config.BuyInAmount = 5000          -- Entry fee per racer
+Config.MinBuyIn = 500
+Config.MaxBuyIn = 5000          -- Entry fee per racer
 Config.MinDistance = 1000          -- Minimum checkpoint distance
 Config.MaxDistance = 1500          -- Maximum checkpoint distance
 Config.TaxPercent = 0.15           -- Server tax cut (0.0 to 1.0)
