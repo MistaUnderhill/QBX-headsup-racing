8315bbb3252f216552fe548b0f58cc96fe0218c5
MistaUnderhill <110700115+MistaUnderhill@users.noreply.github.com>
2025-07-23 15:02:30 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,22 +1,17 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Mistaunderhill'
-description 'Multiplayer Street Race Script for QBX'
-version '2.0.0'
+description 'QBX Street Racing Script'
+author 'Your Name'
+version '1.0.0'
+
+lua54 'yes'
 
 shared_script 'config.lua'
 
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'server.lua',
-}
+client_script 'client.lua'
+server_script 'server.lua'
 
-client_scripts {
-    'client.lua',
-}
+dependency 'qbx-core'
 
-dependencies {
-    'oxmysql',
-    'qbx-core'
-}
+game_type 'street-race'
