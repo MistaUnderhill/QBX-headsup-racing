ebe1ca3e585053f7cc7248838cfcaf3bddf9e1c7
MistaUnderhill <110700115+MistaUnderhill@users.noreply.github.com>
2025-07-23 15:02:56 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,76 +1,95 @@
 local QBCore = exports['qbx-core']:GetCoreObject()
-local inRace = false
-local inCountdown = false
-local checkpoint = nil
-local raceBlip = nil
+local isInvited = false
+local raceCheckpoint = nil
 
-RegisterNetEvent('streetrace:notify', function(msg)
-    QBCore.Functions.Notify(msg, "primary")
-end)
+RegisterNetEvent('qbx-street-racing:inviteToRace', function(data)
+    if isInvited then return end
+    isInvited = true
 
-RegisterNetEvent('streetrace:lockPlayer', function(lock)
-    local ped = PlayerPedId()
-    FreezeEntityPosition(ped, lock)
-end)
-
-RegisterNetEvent('streetrace:startCountdown', function(seconds)
-    inCountdown = true
-    Citizen.CreateThread(function()
-        local count = seconds
-        while count > 0 do
-            Citizen.Wait(0)
-            DrawTxt(0.5, 0.4, 1.0, 1.0, 1.0, tostring(count), 255, 0, 0, 255)
-            Citizen.Wait(1000)
-            count = count - 1
+    local countdown = Config.ConfirmationTimeout
+    CreateThread(function()
+        while countdown > 0 do
+            Wait(1000)
+            countdown -= 1
         end
-        inCountdown = false
+
+        if isInvited then
+            TriggerServerEvent('qbx-street-racing:declineRace')
+            QBCore.Functions.Notify('Race invitation timed out.', 'error')
+            isInvited = false
+        end
+    end)
+
+    CreateThread(function()
+        while isInvited do
+            Wait(0)
+            if IsControlJustPressed(0, 38) then -- E
+                TriggerServerEvent('qbx-street-racing:confirmRace')
+                isInvited = false
+                break
+            end
+        end
+    end)
+
+    QBCore.Functions.Notify("You've been invited to a street race. Press [E] to join.")
+end)
+
+RegisterNetEvent('qbx-street-racing:lockPlayer', function()
+    local player = PlayerPedId()
+    FreezeEntityPosition(player, true)
+end)
+
+RegisterNetEvent('qbx-street-racing:startCountdown', function()
+    local countdown = Config.CountdownTime
+    CreateThread(function()
+        while countdown > 0 do
+            TriggerEvent('chat:addMessage', {
+                args = { tostring(countdown) }
+            })
+            Wait(1000)
+            countdown -= 1
+        end
+        TriggerEvent('chat:addMessage', {
+            args = { 'GO!' }
+        })
+        TriggerServerEvent('qbx-street-racing:unlockPlayer')
     end)
 end)
 
-RegisterNetEvent('streetrace:startRace', function(coords)
-    checkpoint = coords
-    inRace = true
-
-    if raceBlip then
-        RemoveBlip(raceBlip)
-    end
-
-    raceBlip = AddBlipForCoord(coords.x, coords.y, coords.z)
-    SetBlipRoute(raceBlip, true)
-    SetBlipRouteColour(raceBlip, 5)
-
-    QBCore.Functions.Notify("Race started! First to the checkpoint wins!", "success")
+RegisterNetEvent('qbx-street-racing:unlockPlayer', function()
+    local player = PlayerPedId()
+    FreezeEntityPosition(player, false)
 end)
 
-CreateThread(function()
-    while true do
-        Citizen.Wait(500)
-        if inRace and checkpoint and not inCountdown then
-            local ped = PlayerPedId()
-            local dist = #(GetEntityCoords(ped) - checkpoint)
-            if dist < 25.0 then
-                TriggerServerEvent('streetrace:finished')
-                inRace = false
-                if raceBlip then
-                    RemoveBlip(raceBlip)
-                    raceBlip = nil
-                end
+RegisterNetEvent('qbx-street-racing:setCheckpoint', function(coords)
+    if DoesBlipExist(raceCheckpoint) then
+        RemoveBlip(raceCheckpoint)
+    end
+    raceCheckpoint = AddBlipForCoord(coords.x, coords.y, coords.z)
+    SetBlipRoute(raceCheckpoint, true)
+    SetBlipRouteColour(raceCheckpoint, 5)
+
+    CreateThread(function()
+        while true do
+            Wait(1000)
+            local playerCoords = GetEntityCoords(PlayerPedId())
+            if #(playerCoords - coords) < 5.0 then
+                TriggerServerEvent('qbx-street-racing:finishRace')
+                RemoveBlip(raceCheckpoint)
+                break
             end
         end
-    end
+    end)
 end)
 
--- Helper function to draw text on screen
-function DrawTxt(x, y, width, height, scale, text, r, g, b, a)
-    SetTextFont(4)
-    SetTextProportional(0)
-    SetTextScale(scale, scale)
-    SetTextColour(r, g, b, a)
-    SetTextDropShadow(0, 0, 0, 0, 255)
-    SetTextEdge(1, 0, 0, 0, 255)
-    SetTextDropShadow()
-    SetTextOutline()
-    SetTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawText(x - width/2, y - height/2)
-end
+RegisterCommand("raceleaderboard", function()
+    TriggerServerEvent("qbx-street-racing:requestLeaderboard")
+end)
+
+RegisterNetEvent("qbx-street-racing:showLeaderboard", function(data)
+    for i, racer in ipairs(data) do
+        TriggerEvent("chat:addMessage", {
+            args = { ("%d. %s - %d wins"):format(i, racer.name, racer.wins) }
+        })
+    end
+end)
